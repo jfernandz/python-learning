aa0ea5020ff507b72a5b50f84abf6a791295b477
Javier Fernández <jfernandz1290@gmail.com>
2020-09-17 14:26:47 +0200

    Testing ideas in Regex Engine

--- Regex_Engine/testing/regex_stage5_nofunc.py
+++ Regex_Engine/testing/regex_stage5_nofunc.py
@@ -19,12 +19,13 @@ def match_string(re, string):
     a given 'string' character by character using match_char()
     function.
     """
-    print(f"--------> {re}|{string}")
+    # print(f"--------> {re}|{string}")  # Debug
+    global plus_flag
     if len(re) == 0:
         # 're' pattern was sliced to the end so this means the 're' pattern
         # WAS found inside 'string'
-        print("'regex' was consumed")
-        print(f"{re}|{string}")
+        # print("'regex' was consumed")  # Debug
+        # print(f"{re}|{string}")  # Debug
         return True
     elif len(string) == 0:
         # 'string' was sliced to the end so this means the 're' pattern
@@ -33,32 +34,35 @@ def match_string(re, string):
             # The string was consumed but 're' patter wasn't, however last
             # character in 're' is "$", which is the special character that
             # points the pattern must be located at the and of the string.
-            print('Last character in "re" was "$"')
+            # print('Last character in "re" was "$"')  # Debug
             return True
-        if len(re) == 2 and re[-1] in ("*", "?", "+"):
-            print('Last character is "*", "?" or "+"')
-            return match_string(re[2:], string)
-        print("'string' was consumed")
+        # if len(re) == 2 and re[-1] in ("*", "?"):
+        #     # print('Last character is "*" or "?"')  # Debug
+        #     return match_string(re[2:], string)
+        # elif len(re) == 2 and re[-1] == "+" and plus_flag is True:
+        #     return match_string(re[2:], string)
+        # print("'string' was consumed")  # Debug
         return False
 
     if match_char(re[0], string[0]):
         if re[1:].startswith("?"):
             return match_string(re[2:], string[1:])
-        elif re[1:].startswith("*") or re[1:].startswith("+"):
+        if re[1:].startswith("*") or re[1:].startswith("+"):
+            if re[1:].startswith("+"):
+                plus_flag = True
             return match_string(re, string[1:])
         else:
-            # print(f"Main:  -> {re}|{string}")  # Debug
-            # print(f"Mai-:  -> {re[1:]}|{string[1:]}")  # Important debug
             return match_string(re[1:], string[1:])
     else:
         if re[1:].startswith("?") or re[1:].startswith("*"):
             return match_string(re[2:], string)
-        # elif re[1:].startswith("+"):
-        #    return match_string(re[2:], string)
-            # print("+False")
-            # return False
+        elif re[1:].startswith("+"):
+            if plus_flag:
+                return match_string(re[2:], string)
+            else:
+                return False
         else:
-            print("uFalse")
+            # print("uFalse")  # Debug
             return False
 
 
@@ -71,7 +75,7 @@ def find_pattern(re, long_str):
         if len(long_str) == 0:
             # long_str was sliced to the end and the 're' pattern
             # WASN'T found inside
-            print("long_str was consumed")
+            # print("long_str was consumed")  # Debug
             return False
         else:
             # It keeps looking the pattern recursively inside long_str
@@ -95,4 +99,6 @@ def main_func():
 
 
 if __name__ == '__main__':
+    global plus_flag
+    plus_flag = False
     main_func()
