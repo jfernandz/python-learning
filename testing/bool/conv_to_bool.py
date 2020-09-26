7cf01942e1e963758485bf25a19f66cf211dd8f4
Javier Fernández <WyRe12@gmail.com>
2020-09-26 22:28:12 +0200

    Interesting problem about boolean operators

--- testing/bool/conv_to_bool.py
+++ testing/bool/conv_to_bool.py
@@ -1,8 +1,15 @@
+# X or Y  --> Y is evaluated only if X is False
+# X and Y --> Y is evaluated only if X is True
+
+# So we need in the if that `a and a[0] < b[0]` just be evaluated only
+# when `not b` is False, i.e. `b` list is not empty. Because if `b` list
+# is empty, b[0] will give an IndexError
+
 def merge_arrays(a, b):
     # "c" will contain the result of merging arrays "a" and "b"
     c = []
     while a or b:
-        if a and a[0] < b[0]:
+        if not b or a and a[0] < b[0]:
             # removing the first element from "a" and adding it to "c"
             c.append(a[0])
             a.pop(0)
@@ -14,7 +21,7 @@ def merge_arrays(a, b):
     print(c)
 
 
-a = [1, 2, 3, 3, 5]
+a = [1, 2, 3, 5, 6]
 b = [2, 3, 4, 4]
 
 
